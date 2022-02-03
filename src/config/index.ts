c2082b5d571b85461fe375c032857e4aa4b13b8e
Thant Sin Toe <mr.thantsintoe@gmail.com>
2022-02-03 11:25:36 +0630

    removed unecessary files and folders. Move configs to /config/index.ts

---
+++ src/config/index.ts
@@ -0,0 +1,135 @@
+import fs from 'fs'
+import path from 'path'
+import merge from 'deepmerge'
+
+const overwriteMerge = (target, source, options) => source
+
+let config = { server: { baseDir: './' } }
+
+if (fs.existsSync(path.join(process.cwd(), 'config.json'))) {
+  const fileConfig = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'config.json')).toString())
+  config = merge(config, fileConfig, { arrayMerge: overwriteMerge })
+}
+
+if (process.env.BASE_DIR) {
+  let baseDir = process.env.BASE_DIR || '.'
+  let baseDirFileConfig
+
+  if (fs.existsSync(path.join(baseDir, 'config.json'))) {
+    baseDirFileConfig = JSON.parse(fs.readFileSync(path.join(baseDir, 'config.json')).toString())
+  }
+  config = merge(config, baseDirFileConfig, { arrayMerge: overwriteMerge })
+  config.server.baseDir = process.env.BASE_DIR
+}
+
+if (process.env.APP_SEEDLIST) {
+  config = merge(
+    config,
+    {
+      server: {
+        p2p: {
+          maxJoinedPerCycle: 5,
+          maxRotatedPerCycle: 0,
+          existingArchivers: [
+            {
+              ip: process.env.APP_SEEDLIST,
+              port: 4000,
+              publicKey: '758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3',
+            },
+          ],
+        },
+        sharding: {
+          nodesPerConsensus: 50,
+        },
+      },
+    },
+    { arrayMerge: overwriteMerge }
+  )
+}
+
+if (process.env.APP_MONITOR) {
+  config = merge(
+    config,
+    {
+      server: {
+        reporting: {
+          recipient: `http://${process.env.APP_MONITOR}:3000/api`,
+        },
+      },
+    },
+    { arrayMerge: overwriteMerge }
+  )
+}
+
+if (process.env.APP_IP) {
+  config = merge(
+    config,
+    {
+      server: {
+        ip: {
+          externalIp: process.env.APP_IP,
+          internalIp: process.env.APP_IP,
+        },
+      },
+    },
+    { arrayMerge: overwriteMerge }
+  )
+}
+
+// Setting minNodesToAllowTxs to 1 allow single node networks
+config = merge(config, {
+  server: {
+    p2p: {
+      minNodesToAllowTxs: 1,
+      minNodes: 50,
+      maxNodes: 50,
+      maxJoinedPerCycle: 6,
+      maxSyncingPerCycle: 12,
+      maxRotatedPerCycle: 1,
+      firstCycleJoin: 10,
+    },
+  },
+})
+
+//rateLimiting settings
+config = merge(config, {
+  server: {
+    rateLimiting: {
+      limitRate: true,
+      loadLimit: {
+        internal: 0.8,
+        external: 0.8,
+        txTimeInQueue: 0.7,
+        queueLength: 0.8,
+      },
+    }
+  }
+})
+
+config = merge(config, {
+  server: {
+    sharding: {
+      nodesPerConsensusGroup: 20,
+    },
+  },
+})
+
+//some debug settings
+config = merge(
+  config,
+  {
+    server: {
+      mode: 'debug',
+      debug: {
+        startInFatalsLogMode: true, //true setting good for big aws test with nodes joining under stress.
+        startInErrorLogMode: false,
+        fakeNetworkDelay: 0,
+        disableSnapshots: true,
+        countEndpointStart: -1,
+      },
+    },
+  },
+  { arrayMerge: overwriteMerge }
+)
+
+export default config
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-import merge from 'deepmerge'
 import stringify from 'fast-json-stable-stringify'
 import * as crypto from 'shardus-crypto-utils'
 import {Account, Address, BN, bufferToHex, toBuffer} from 'ethereumjs-util'
@@ -21,150 +18,11 @@ import * as ShardeumFlags from './shardeum/shardeumFlags'
 import * as WrappedEVMAccountFunctions from './shardeum/wrappedEVMAccountFunctions'
 import {fixDeserializedWrappedEVMAccount, updateEthAccountHash} from './shardeum/wrappedEVMAccountFunctions'
 import {zeroAddressStr} from "./utils";
+import config from './config'
 
 let { shardusFactory } = require('shardus-global-server')
 
 crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc')
-
-/***
- *     ######   #######  ##    ## ######## ####  ######
- *    ##    ## ##     ## ###   ## ##        ##  ##    ##
- *    ##       ##     ## ####  ## ##        ##  ##
- *    ##       ##     ## ## ## ## ######    ##  ##   ####
- *    ##       ##     ## ##  #### ##        ##  ##    ##
- *    ##    ## ##     ## ##   ### ##        ##  ##    ##
- *     ######   #######  ##    ## ##       ####  ######
- */
-
-const overwriteMerge = (target, source, options) => source
-
-let config = { server: { baseDir: './' } }
-
-if (fs.existsSync(path.join(process.cwd(), 'config.json'))) {
-  const fileConfig = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'config.json')).toString())
-  config = merge(config, fileConfig, { arrayMerge: overwriteMerge })
-}
-
-if (process.env.BASE_DIR) {
-  let baseDir = process.env.BASE_DIR || '.'
-  let baseDirFileConfig
-
-  if (fs.existsSync(path.join(baseDir, 'config.json'))) {
-    baseDirFileConfig = JSON.parse(fs.readFileSync(path.join(baseDir, 'config.json')).toString())
-  }
-  config = merge(config, baseDirFileConfig, { arrayMerge: overwriteMerge })
-  config.server.baseDir = process.env.BASE_DIR
-}
-
-if (process.env.APP_SEEDLIST) {
-  config = merge(
-    config,
-    {
-      server: {
-        p2p: {
-          maxJoinedPerCycle: 5,
-          maxRotatedPerCycle: 0,
-          existingArchivers: [
-            {
-              ip: process.env.APP_SEEDLIST,
-              port: 4000,
-              publicKey: '758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3',
-            },
-          ],
-        },
-        sharding: {
-          nodesPerConsensus: 50,
-        },
-      },
-    },
-    { arrayMerge: overwriteMerge }
-  )
-}
-
-if (process.env.APP_MONITOR) {
-  config = merge(
-    config,
-    {
-      server: {
-        reporting: {
-          recipient: `http://${process.env.APP_MONITOR}:3000/api`,
-        },
-      },
-    },
-    { arrayMerge: overwriteMerge }
-  )
-}
-
-if (process.env.APP_IP) {
-  config = merge(
-    config,
-    {
-      server: {
-        ip: {
-          externalIp: process.env.APP_IP,
-          internalIp: process.env.APP_IP,
-        },
-      },
-    },
-    { arrayMerge: overwriteMerge }
-  )
-}
-
-// Setting minNodesToAllowTxs to 1 allow single node networks
-config = merge(config, {
-  server: {
-    p2p: {
-      minNodesToAllowTxs: 1,
-      minNodes: 50,
-      maxNodes: 50,
-      maxJoinedPerCycle: 6,
-      maxSyncingPerCycle: 12,
-      maxRotatedPerCycle: 1,
-      firstCycleJoin: 10,
-    },
-  },
-})
-
-//rateLimiting settings
-config = merge(config, {
-  server: {
-    rateLimiting: {
-      limitRate: true,
-      loadLimit: {
-        internal: 0.8,
-        external: 0.8,
-        txTimeInQueue: 0.7,
-        queueLength: 0.8,
-      },
-    }
-  }
-})
-
-config = merge(config, {
-  server: {
-    sharding: {
-      nodesPerConsensusGroup: 20,
-    },
-  },
-})
-
-//some debug settings
-config = merge(
-  config,
-  {
-    server: {
-      mode: 'debug',
-      debug: {
-        startInFatalsLogMode: true, //true setting good for big aws test with nodes joining under stress.
-        startInErrorLogMode: false,
-        fakeNetworkDelay: 0,
-        disableSnapshots: true,
-        countEndpointStart: -1,
-      },
-    },
-  },
-  { arrayMerge: overwriteMerge }
-)
 
 const shardus = shardusFactory(config)
 
