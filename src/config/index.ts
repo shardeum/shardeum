9a887285baef6c2db22cb203fa11cce06ac3e906
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2022-05-21 08:22:22 -0500

    make sure old snapshot is off

--- src/config/index.ts
+++ src/config/index.ts
@@ -130,7 +130,7 @@ config = merge(
         startInFatalsLogMode: false, // true setting good for big aws test with nodes joining under stress.
         startInErrorLogMode: true,
         fakeNetworkDelay: 0,
-        disableSnapshots: false,// //dont check in if set to true
+        disableSnapshots: true,// //dont check in if set to true
         countEndpointStart: -1,
         hashedDevAuth: '117151a0d719f329a4a570b4ccde4bbb1ec12fa6a7a870a1471ed170e7f08d70', //todo remove this after alpha 1.0
         devPublicKey:'774491f80f47fedb119bb861601490f42bc3ea3b57fc63906c0d08e6d777a592'
