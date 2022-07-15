893d8cedda2331fe2acc187ac9b90c332aac31e8
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2022-07-14 23:17:15 -0500

    point to latest shardus core fixes. minor helper script added

---
+++ src/tests/prettyPrint.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs');
+const path = require('path')
+
+const myArgs = process.argv.slice(2)
+const directory = myArgs[0] //`paste path here`;
+
+//script to pretty print a folder of json files
+fs.readdir(directory, (err, files) => {
+    files.forEach(file => {
+        try{
+            let filepath = path.resolve(directory, file);
+            let fileText = fs.readFileSync(filepath)
+            let fileObj = JSON.parse(fileText)
+            console.log(file);
+
+            var prettyPrint = JSON.stringify(fileObj,null,2);  
+            let filepath2 = path.resolve(directory, file + '.update.json');
+            console.log(filepath2);
+            fs.writeFileSync(filepath2,prettyPrint)
+        } catch(error) {
+            //console.log(error);
+        }
+    });
+});
