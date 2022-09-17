f358b41ace58827e27dc4184624bb04e67bfa457
kgmyatthu <2962000kaungmyatthu@gmail.com>
2022-09-17 14:02:39 +0630

    fix: improve the test scripts

--- test/scaleTest.ts
+++ test/scaleTest.ts
@@ -2,7 +2,9 @@ import execa from 'execa'
 import * as utils from './testUtils'
 import { join } from 'path'
 import { kill } from 'process'
-
+const {
+  performance,
+} = require('perf_hooks');
 const opts = { shell: true }
 const LOAD_TESTER = join(__dirname, '../../load-tester') // spam-client repo path
 // const abortController = new AbortController()
@@ -35,8 +37,11 @@ export async function waitForNetworkLoad(load, value) {
   return true
 }
 
-export async function waitForNetworkScaling(desired) {
+// desired is node count, and _timeout is maximum time this function will wait until it return false meaning test has failed.
+// negative number in _timeout will result in infinte waiting time
+export async function waitForNetworkScaling(desired: number, _timeout: number = -1) {
   let isCriteriaMet = false
+  const start = performance.now();
   while (!isCriteriaMet) {
     try {
       let activeNodes = await utils.queryActiveNodes()
@@ -44,6 +49,14 @@ export async function waitForNetworkScaling(desired) {
       else await utils._sleep(10000)
     } catch (e) {
       await utils._sleep(30000)
+    } finally{
+      if(_timeout < 0 ) continue;
+      const now = performance.now();
+      const elapsed = Math.floor(now - start) / 60000;
+      console.log(elapsed, "min");
+      if(_timeout > 0 && _timeout < elapsed) {
+        return false; //timeout!!!,  network has failed to scale within the max time, returning false;
+      }
     }
   }
   return true
@@ -60,7 +73,7 @@ export const scaleTest = (START_NETWORK_SIZE: number) => {
 
     console.log('Waiting for network to scale up...')
 
-    let hasNetworkScaledUp = await waitForNetworkScaling(START_NETWORK_SIZE * 2)
+    let hasNetworkScaledUp = await waitForNetworkScaling(START_NETWORK_SIZE * 2, 30)
     spamProcess.cancel()
 
     // try {
@@ -85,7 +98,7 @@ export const scaleTest = (START_NETWORK_SIZE: number) => {
     console.log('TEST: Auto scale down the network successfully')
 
     let isLoadDecreased = await waitForNetworkLoad('low', 0.2)
-    let hasNetworkScaledDown = await waitForNetworkScaling(START_NETWORK_SIZE)
+    let hasNetworkScaledDown = await waitForNetworkScaling(START_NETWORK_SIZE, 30)
 
     expect(hasNetworkScaledDown).toBe(true)
     expect(isLoadDecreased).toBe(true)
@@ -93,10 +106,9 @@ export const scaleTest = (START_NETWORK_SIZE: number) => {
 
   test('Data is correctly synced across the nodes after network scaled down', async () => {
     console.log('TEST: Data is correctly synced across the nodes after network scaled down')
-    let result = await utils.getInsyncAll()
-    const in_sync = result.in_sync === START_NETWORK_SIZE
-    const out_sync = result.out_sync === 0
-    expect(in_sync).toBe(true)
-    expect(out_sync).toBe(true)
+    let { in_sync, out_sync } = await utils.getInsyncAll()
+
+    expect(in_sync === START_NETWORK_SIZE).toBe(true)
+    expect(out_sync === 0).toBe(true)
   })
 }
