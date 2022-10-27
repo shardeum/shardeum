636e6d8d73364a131725cec60d4ad3aedaf64026
Arham Jain <arham.nec@gmail.com>
2022-10-27 16:18:40 +0530

    style(index): reformatted code

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ export let genesisAccounts = []
 const ERC20_BALANCEOF_CODE = '0x70a08231'
 
 const shardus = shardusFactory(config, {
-  customStringifier: SerializeToJsonString
+  customStringifier: SerializeToJsonString,
 })
 
 // const pay_address = '0x50F6D9E5771361Ec8b95D6cfb8aC186342B70120' // testing account for node_reward
