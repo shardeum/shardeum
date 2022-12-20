5732578d8860125cc89ec48df180306c7fc9c7cc
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2022-12-20 07:45:34 -0600

    set balance precheck flag to true

--- src/shardeum/shardeumFlags.ts
+++ src/shardeum/shardeumFlags.ts
@@ -71,7 +71,7 @@ export let ShardeumFlags: ShardeumFlags = {
   DebugRestoreArchiveBatch: 2000,
   CheckNonce: true,
   txNoncePreCheck: true,
-  txBalancePreCheck: false,
+  txBalancePreCheck: true,
   autoGenerateAccessList: true,
   forwardGenesisAccounts: true,
   UseDBForAccounts: true,
