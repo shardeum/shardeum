f44e4167cee3d30d98e509b969f8f9d82878ca37
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-01-04 20:51:48 -0600

    turn staking flag back off so we can merge to dev

--- src/shardeum/shardeumFlags.ts
+++ src/shardeum/shardeumFlags.ts
@@ -103,7 +103,7 @@ export let ShardeumFlags: ShardeumFlags = {
     ['tx/:hash']: 5,
   },
   generateMemoryPatternData: true,
-  StakingEnabled: true,
+  StakingEnabled: false,
   MinStakeCertSig: 1, // this is the minimum amount of signature required for stake certification. will move to network param in future.
   FullCertChecksEnabled: true,
 }
