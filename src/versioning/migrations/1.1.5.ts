8e204d7d9d690d70ad62dc7db9278babefd58cd9
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-03-22 03:29:59 -0500

    roll latest fix into a 1.1.5 update

--- src/versioning/migrations/1.1.5.ts
+++ src/versioning/migrations/1.1.5.ts
@@ -1,4 +1,5 @@
 import { nestedCountersInstance } from '@shardus/core'
+import { ShardeumFlags } from '../../shardeum/shardeumFlags'
 import { Migration } from '../types'
 // import { shardusConfig } from '../..'
 
@@ -7,6 +8,7 @@ export const migrate: Migration = async () => {
   console.log('migrate 1.1.5')
   nestedCountersInstance.countEvent('migrate-1.1.5', 'calling migrate 1.1.5')
 
+  ShardeumFlags.shardeumTimeout = 50000
   // this is from reverse migrate.  keeping the comments for local testing but will turn in to real migration code later
   //shardusConfig.features.archiverDataSubscriptionsUpdate = false
 }
