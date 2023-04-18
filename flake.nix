272b6489d68d5811427efd4f37d5c852d9eecb70
municorn <municorn@musicaloft.com>
2023-04-17 19:23:24 -0500

    Add nix flake
    mostly for development environment on NixOS
    
    flake: Add vscode-langservers-extracted
    
    for eslint LSP
    
    flake: Remove package and app derivations
    
    flake: Remove Rust

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Shardeum server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    shardus-cli.url = "git+https://gitlab.com/shardus/tools/shardus-cli?ref=nix-flake";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    shardus-cli,
+    utils,
+  }: let
+    appName = "shardeum-server";
+    out =
+      utils.lib.eachDefaultSystem
+      (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        buildNodeJs = pkgs.callPackage "${nixpkgs}/pkgs/development/web/nodejs/nodejs.nix" {python = pkgs.python3;};
+        custom-nodejs = buildNodeJs {
+          enableNpm = true;
+          version = "16.11.1";
+          sha256 = "0y32mdv8zs35la2bny8d9rxjvj1vr8z079ji1g6ajc2yw96pyn37";
+        };
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          custom-nodejs
+        ];
+        buildInputs = with pkgs; [];
+      in {
+        # `nix develop` or direnv
+        devShell = pkgs.mkShell {
+          packages =
+            nativeBuildInputs
+            ++ buildInputs
+            ++ (with pkgs; [
+              nodePackages.typescript-language-server
+              nodePackages.vscode-langservers-extracted
+              nodePackages.prettier
+
+              shardus-cli.packages.${system}.default
+            ]);
+        };
+      });
+  in
+    out
+    // {
+      overlay = final: prev: {
+        ${appName} = self.defaultPackage.${prev.system};
+      };
+    };
+}
