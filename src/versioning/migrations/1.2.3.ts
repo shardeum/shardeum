7c8f58f81ece267cdf8eb9a3ccc38f48fa45e87b
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-05-18 17:50:11 -0500

    past migration for 1.2.3

--- src/versioning/migrations/1.2.3.ts
+++ src/versioning/migrations/1.2.3.ts
@@ -1,12 +1,22 @@
 import { nestedCountersInstance } from '@shardus/core'
 import { ShardeumFlags } from '../../shardeum/shardeumFlags'
 import { Migration } from '../types'
-// import { shardusConfig } from '../..'
+import { shardusConfig } from '../..'
 
 export const migrate: Migration = async () => {
-  console.log('migrate 1.1.8')
+  console.log('migrate 1.2.3')
 
   ShardeumFlags.fixContractBytes = true
   ShardeumFlags.fixCertExpTiming = true
   nestedCountersInstance.countEvent('migrate-1.1.8', 'calling migrate 1.1.8')
+
+  //new logic to prevent already active nodes from submitting active requests
+  shardusConfig.p2p.validateActiveRequests = true
+
+  //Allow nodes to contineue on unhandled exceptions if the network is low on nodes
+  shardusConfig.p2p.continueOnException = true
+
+  //not sure yet if this is safe to set.
+  //This is a major performance upgrade for p2p tell
+  shardusConfig.p2p.useSignaturesForAuth = true
 }
