3065d8623548fa37e5ecdc9a6f2ebd2077853d22
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-07-18 23:54:14 -0500

    enable 1.5.2 migration

--- src/versioning/index.ts
+++ src/versioning/index.ts
@@ -31,6 +31,7 @@ export const onActiveVersionChange = async (newActiveVersion: string) => {
     '1.3.1',
     '1.4.1',
     '1.4.2',
+    '1.5.2'
   ]
 
   for (let index = 0; index < migrations.length; index++) {
