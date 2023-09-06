7e58cc05372f5c6e2e1decd92f0b8e724e4922b7
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-09-06 16:46:51 -0500

    correct a setting

--- src/shardeum/initialNetworkParameters.ts
+++ src/shardeum/initialNetworkParameters.ts
@@ -17,7 +17,7 @@ export const initialNetworkParamters: NetworkParameters = {
   maintenanceFee: 0,
   minVersion: '1.5.2.1',
   activeVersion: '1.5.2',
-  latestVersion: '1.5.2.2',
+  latestVersion: '1.5.6',
   archiver: {
     minVersion: '3.3.5',
     activeVersion: '3.3.5',
