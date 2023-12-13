30870f2cdea5424e02a03ad8ef18ac7b5cce5df9
municorn <municorn@musicaloft.com>
2023-12-13 16:31:46 -0600

    feat(dao): migrate accounts api from liberdus

--- src/dao/api/accounts/address.ts
+++ src/dao/api/accounts/address.ts
@@ -1,10 +1,17 @@
+import { Shardus } from '@shardus/core'
+import { Request, Response } from 'express'
 import { getShardusAPI } from '../../../index'
 
-export const address = dapp => async (req, res): Promise<void> => {
+export const address = (dapp: Shardus) => async (req: Request, res: Response): Promise<void> => {
   try {
     const name = req.params['name']
     const account = await getShardusAPI().getLocalOrRemoteAccount(name)
-    if (account && account.data) {
+    if (
+      "data" in account
+      && typeof account.data == "object"
+      && "address" in account.data
+      && account.data.address
+    ) {
       // to-do: type of account and account.data
       res.json({ address: account.data.address })
     } else {
---
+++ src/dao/api/accounts/tollOfFriend.ts
@@ -0,0 +1,68 @@
+import { Shardus } from '@shardus/core'
+import { Request, Response } from 'express'
+import { getShardusAPI } from '../../../index'
+import { networkAccount } from '../../../shardeum/shardeumConstants'
+
+export const tollOfFriend = (dapp: Shardus) => async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = req.params['id']
+    const friendId = req.params['friendId']
+    if (!id) {
+      res.json({
+        error: 'No provided id in the route: account/:id/:friendId/toll',
+      })
+    }
+    if (!friendId) {
+      res.json({
+        error: 'No provided friendId in the route: account/:id/:friendId/toll',
+      })
+    }
+    const account = await getShardusAPI().getLocalOrRemoteAccount(id)
+    if (account) {
+      if (
+        "data" in account
+        && typeof account.data === "object"
+        && "data" in account.data
+        && typeof account.data.data === "object"
+        && "friends" in account.data.data
+        && account.data.data.friends[friendId]
+      ) {
+        res.json({ toll: 0 })
+      } else {
+        const accountDataToll = "data" in account
+          && typeof account.data === "object"
+          && "data" in account.data
+          && typeof account.data.data === "object"
+          && "toll" in account.data.data
+          ? account.data.data.toll : undefined;
+
+        if (accountDataToll == null) {
+          const network = await getShardusAPI().getLocalOrRemoteAccount(networkAccount)
+
+          const toll =
+            typeof network.data === "object"
+              && "current" in network.data
+              && typeof network.data.current === "object"
+              && "defaultToll" in network.data.current
+              ? network.data.current.defaultToll : undefined;
+
+          res.json({ toll })
+        } else {
+          const toll =
+            typeof account.data === "object"
+              && "data" in account.data
+              && typeof account.data.data === "object"
+              && "toll" in account.data.data
+              ? account.data.data.toll : undefined;
+
+          res.json({ toll })
+        }
+      }
+    } else {
+      res.json({ error: 'No account with the given id' })
+    }
+  } catch (error) {
+    dapp.log(error)
+    res.json({ error })
+  }
+}
