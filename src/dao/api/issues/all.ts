d294a9d46b5e22c293ee4fb5057080f280fb48c0
municorn <municorn@musicaloft.com>
2023-12-13 17:02:28 -0600

    feat(dao): migrate issues api from liberdus

--- src/dao/api/issues/all.ts
+++ src/dao/api/issues/all.ts
@@ -1,15 +1,18 @@
-import * as configs from '../../../config'
 import * as AccountsStorage from '../../../storage/accountStorage'
 import * as crypto from '@shardus/crypto-utils'
 import { getShardusAPI } from '../../../index'
 
-getShardusAPI().registerExternalGet('all', async (req, res) => {
+getShardusAPI().registerExternalGet('all', async (_req, res) => {
   try {
     const network = AccountsStorage.cachedNetworkAccount
     const issues = []
-    for (let i = 1; i <= network.data.issue; i++) {
+    const issue = "data" in network
+      && typeof network.data === "object"
+      && "issue" in network.data
+      ? network.data.issue : 0;
+    for (let i = 1; i <= issue; i++) {
       const issue = await getShardusAPI().getLocalOrRemoteAccount(crypto.hash(`issue-${i}`))
-      if (issue && issue.data) {
+      if (issue?.data) {
         issues.push(issue.data)
       }
     }
