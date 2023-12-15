f96de17510938388fc6a458c22e8f3a64fedd850
municorn <municorn@musicaloft.com>
2023-12-15 16:24:09 -0600

    feat(dao): migrate proposals api from liberdus

---
+++ src/dao/api/proposals/count.ts
@@ -0,0 +1,26 @@
+import * as crypto from '@shardus/crypto-utils'
+import { getShardusAPI } from '../../../index'
+import { networkAccount } from '../../../shardeum/shardeumConstants'
+import { Shardus } from '@shardus/core'
+import { Request, Response } from 'express'
+
+export const count = (dapp: Shardus) => async (_req: Request, res: Response): Promise<void> => {
+  try {
+    const network = await getShardusAPI().getLocalOrRemoteAccount(networkAccount)
+
+    const issueCount =
+      typeof network.data == "object"
+        && "issue" in network.data
+        ? network.data.issue : undefined;
+    const issue = await getShardusAPI().getLocalOrRemoteAccount(crypto.hash(`issue-${issueCount}`))
+
+    const count =
+      typeof issue?.data == "object"
+        && "proposalCount" in issue.data
+        ? issue.data.proposalCount : undefined;
+    res.json({ count })
+  } catch (error) {
+    dapp.log(error)
+    res.json({ error })
+  }
+}
