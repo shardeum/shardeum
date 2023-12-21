8ee4dec4fa5c9143b75a753143775e942f26633d
municorn <municorn@musicaloft.com>
2023-12-21 17:12:17 -0600

    feat(dao): implement DaoTx for ApplyDevPayment

--- src/dao/tx/apply_developer_payment.ts
+++ src/dao/tx/apply_developer_payment.ts
@@ -4,7 +4,8 @@ import { daoConfig } from '../../config/dao'
 import { DeveloperPayment } from '../types'
 import { TransactionKeys, WrappedStates } from '../../shardeum/shardeumTypes'
 import { DaoGlobalAccount } from '../accounts/networkAccount'
-import { WrappedResponse } from '@shardus/core/dist/shardus/shardus-types'
+import { IncomingTransactionResult, WrappedResponse } from '@shardus/core/dist/shardus/shardus-types'
+import { DaoTx } from '.'
 
 export interface ApplyDevPayment {
   type: 'apply_dev_payment'
@@ -12,37 +13,39 @@ export interface ApplyDevPayment {
   developerFund: DeveloperPayment[]
 }
 
-export function validateFields(tx: ApplyDevPayment, response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
-  if (!Array.isArray(tx.developerFund)) {
-    response.success = false
-    response.reason = 'tx "developerFund" field must be an array.'
-    throw new Error(response.reason)
+export class ApplyDevPayment implements DaoTx<DaoGlobalAccount> {
+  validateFields(this: ApplyDevPayment, response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
+    if (!Array.isArray(this.developerFund)) {
+      response.success = false
+      response.reason = 'tx "developerFund" field must be an array.'
+      throw new Error(response.reason)
+    }
+    return response
   }
-  return response
-}
 
-export function validate(response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
-  response.success = true
-  response.reason = 'This transaction is valid!'
-  return response
-}
-
-export function apply(tx: ApplyDevPayment, txTimestamp: number, wrappedStates: WrappedStates, dapp: Shardus): void {
-  const network: DaoGlobalAccount = wrappedStates[daoConfig.daoAccount].data
-  network.developerFund = tx.developerFund
-  network.timestamp = txTimestamp
-  dapp.log(`=== APPLIED DEV_PAYMENT GLOBAL ${stringify(network)} ===`)
-}
-
-export function keys(result: TransactionKeys): TransactionKeys {
-  result.targetKeys = [daoConfig.daoAccount]
-  result.allKeys = [...result.sourceKeys, ...result.targetKeys]
-  return result
-}
-
-export function createRelevantAccount(dapp: Shardus, account: DaoGlobalAccount, accountId: string, accountCreated = false): WrappedResponse {
-  if (!account) {
-    throw new Error('Network Account must already exist for the apply_developer_payment transaction')
+  validate(_wrappedStates: WrappedStates, response: IncomingTransactionResult): IncomingTransactionResult {
+    response.success = true
+    response.reason = 'This transaction is valid!'
+    return response
   }
-  return dapp.createWrappedResponse(accountId, accountCreated, account.hash, account.timestamp, account)
+
+  apply(this: ApplyDevPayment, txTimestamp: number, wrappedStates: WrappedStates, dapp: Shardus): void {
+    const network: DaoGlobalAccount = wrappedStates[daoConfig.daoAccount].data
+    network.developerFund = this.developerFund
+    network.timestamp = txTimestamp
+    dapp.log(`=== APPLIED DEV_PAYMENT GLOBAL ${stringify(network)} ===`)
+  }
+
+  keys(result: TransactionKeys): TransactionKeys {
+    result.targetKeys = [daoConfig.daoAccount]
+    result.allKeys = [...result.sourceKeys, ...result.targetKeys]
+    return result
+  }
+
+  createRelevantAccount(dapp: Shardus, account: DaoGlobalAccount, accountId: string, accountCreated = false): WrappedResponse {
+    if (!account) {
+      throw new Error('Network Account must already exist for the apply_developer_payment transaction')
+    }
+    return dapp.createWrappedResponse(accountId, accountCreated, account.hash, account.timestamp, account)
+  }
 }
