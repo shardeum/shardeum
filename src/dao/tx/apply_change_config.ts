8df5ce3b249e618f7b94157b86ba25e9cee5d452
municorn <municorn@musicaloft.com>
2023-12-21 16:42:14 -0600

    feat(dao): implement DaoTx for ApplyChangeConfig

--- src/dao/tx/apply_change_config.ts
+++ src/dao/tx/apply_change_config.ts
@@ -2,13 +2,13 @@ import stringify from 'fast-stable-stringify'
 import { Shardus, ShardusTypes } from '@shardus/core'
 import { create } from '../accounts'
 import { daoConfig } from '../../config/dao'
-import { TransactionKeys, WrappedResponse } from '@shardus/core/dist/shardus/shardus-types'
+import { IncomingTransactionResult, TransactionKeys, WrappedResponse } from '@shardus/core/dist/shardus/shardus-types'
 import { NodeAccount } from '../accounts/nodeAccount'
 import { NetworkAccount, WrappedStates } from '../../shardeum/shardeumTypes'
 import { Change } from '@shardus/types/build/src/p2p/CycleParserTypes'
+import { DaoTx } from '.'
 
 export interface ApplyChangeConfig {
-  type: 'apply_change_config'
   change: {
     cycle: number
     change: Change
@@ -16,33 +16,35 @@ export interface ApplyChangeConfig {
   timestamp: number
 }
 
-export function validateFields(response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
-  return response
-}
-
-export function validate(response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
-  response.success = true
-  response.reason = 'This transaction is valid!'
-  return response
-}
-
-export function apply(tx: ApplyChangeConfig, txTimestamp: number, wrappedStates: WrappedStates, dapp: Shardus): void {
-  const network: NetworkAccount = wrappedStates[daoConfig.daoAccount].data
-  network.listOfChanges.push(tx.change)
-  network.timestamp = txTimestamp
-  dapp.log(`=== APPLIED CHANGE_CONFIG GLOBAL ${stringify(network)} ===`)
-}
-
-export function keys(result: TransactionKeys): TransactionKeys {
-  result.targetKeys = [daoConfig.daoAccount]
-  result.allKeys = [...result.sourceKeys, ...result.targetKeys]
-  return result
-}
-
-export function createRelevantAccount(dapp: Shardus, account: NodeAccount, accountId: string, accountCreated = false): WrappedResponse {
-  if (!account) {
-    account = create.nodeAccount(accountId)
-    accountCreated = true
+export class ApplyChangeConfig implements DaoTx<NodeAccount> {
+  validateFields(response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
+    return response
   }
-  return dapp.createWrappedResponse(accountId, accountCreated, account.hash, account.timestamp, account)
+
+  validate(_wrappedStates: WrappedStates, response: IncomingTransactionResult): IncomingTransactionResult {
+    response.success = true
+    response.reason = 'This transaction is valid!'
+    return response
+  }
+
+  apply(txTimestamp: number, wrappedStates: WrappedStates, dapp: Shardus): void {
+    const network: NetworkAccount = wrappedStates[daoConfig.daoAccount].data
+    network.listOfChanges.push(this.change)
+    network.timestamp = txTimestamp
+    dapp.log(`=== APPLIED CHANGE_CONFIG GLOBAL ${stringify(network)} ===`)
+  }
+
+  keys(result: TransactionKeys): TransactionKeys {
+    result.targetKeys = [daoConfig.daoAccount]
+    result.allKeys = [...result.sourceKeys, ...result.targetKeys]
+    return result
+  }
+
+  createRelevantAccount(dapp: Shardus, account: NodeAccount, accountId: string, accountCreated = false): WrappedResponse {
+    if (!account) {
+      account = create.nodeAccount(accountId)
+      accountCreated = true
+    }
+    return dapp.createWrappedResponse(accountId, accountCreated, account.hash, account.timestamp, account)
+  }
 }
