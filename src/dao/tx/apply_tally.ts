333ac6a77d14805cc51a782280f9b2f9e208e985
municorn <municorn@musicaloft.com>
2023-12-21 17:19:23 -0600

    feat(dao): implement DaoTx for ApplyTally

--- src/dao/tx/apply_tally.ts
+++ src/dao/tx/apply_tally.ts
@@ -5,8 +5,9 @@ import _ from 'lodash'
 import { daoConfig } from '../../config/dao'
 import { DaoGlobalAccount } from '../accounts/networkAccount'
 import { NetworkParameters, TransactionKeys, WrappedStates } from '../../shardeum/shardeumTypes'
-import { WrappedResponse } from '@shardus/core/dist/shardus/shardus-types'
+import { IncomingTransactionResult, WrappedResponse } from '@shardus/core/dist/shardus/shardus-types'
 import { Windows } from '../types'
+import { DaoTx } from '.'
 
 export interface ApplyTally {
   type: 'apply_tally'
@@ -15,83 +16,87 @@ export interface ApplyTally {
   nextWindows: Windows
 }
 
-export function validateFields(tx: ApplyTally, response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
-  if (_.isEmpty(tx.next) || typeof tx.next !== 'object') {
-    response.success = false
-    response.reason = 'tx "next" field must be a non empty object'
-    throw new Error(response.reason)
-  }
-  if (_.isEmpty(tx.nextWindows) || typeof tx.nextWindows !== 'object') {
-    response.success = false
-    response.reason = 'tx "nextWindows" field must be a non empty object'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.title !== 'string') {
-    response.success = false
-    response.reason = 'tx "next parameter title" field must be a string.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.description !== 'string') {
-    response.success = false
-    response.reason = 'tx "next parameter description" field must be a string.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.nodeRewardInterval !== 'number') {
-    response.success = false
-    response.reason = 'tx "next parameter nodeRewardInterval" field must be a number.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.nodeRewardAmountUsd !== 'bigint') {
-    response.success = false
-    response.reason = 'tx "next parameter nodeRewardAmountUsd" field must be a bigint.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.nodePenaltyUsd !== 'bigint') {
-    response.success = false
-    response.reason = 'tx "next parameter nodePenaltyUsd" field must be a bigint.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.stakeRequiredUsd !== 'bigint') {
-    response.success = false
-    response.reason = 'tx "next parameter stakeRequiredUsd" field must be a bigint.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.maintenanceInterval !== 'number') {
-    response.success = false
-    response.reason = 'tx "next parameter maintenanceInterval" field must be a number.'
-    throw new Error(response.reason)
-  }
-  if (typeof tx.next.maintenanceFee !== 'number') {
-    response.success = false
-    response.reason = 'tx "next parameter maintenanceFee" field must be a number.'
-    throw new Error(response.reason)
-  }
-  return response
-}
+export class ApplyTally implements DaoTx<DaoGlobalAccount> {
 
-export function validate(response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
-  response.success = true
-  response.reason = 'This transaction is valid!'
-  return response
-}
 
-export function apply(tx: ApplyTally, txTimestamp: number, wrappedStates: WrappedStates, dapp: Shardus): void {
-  const network: DaoGlobalAccount = wrappedStates[daoConfig.daoAccount].data
-  network.next = tx.next
-  network.nextWindows = tx.nextWindows
-  network.timestamp = txTimestamp
-  dapp.log(`APPLIED TALLY GLOBAL ${stringify(network)} ===`)
-}
-
-export function keys(result: TransactionKeys): TransactionKeys {
-  result.targetKeys = [daoConfig.daoAccount]
-  result.allKeys = [...result.sourceKeys, ...result.targetKeys]
-  return result
-}
-
-export function createRelevantAccount(dapp: Shardus, account: DaoGlobalAccount, accountId: string, accountCreated = false): WrappedResponse {
-  if (!account) {
-    throw new Error('Network Account must already exist for the apply_tally transaction')
+  validateFields(this: ApplyTally, response: ShardusTypes.IncomingTransactionResult): ShardusTypes.IncomingTransactionResult {
+    if (_.isEmpty(this.next) || typeof this.next !== 'object') {
+      response.success = false
+      response.reason = 'tx "next" field must be a non empty object'
+      throw new Error(response.reason)
+    }
+    if (_.isEmpty(this.nextWindows) || typeof this.nextWindows !== 'object') {
+      response.success = false
+      response.reason = 'tx "nextWindows" field must be a non empty object'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.title !== 'string') {
+      response.success = false
+      response.reason = 'tx "next parameter title" field must be a string.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.description !== 'string') {
+      response.success = false
+      response.reason = 'tx "next parameter description" field must be a string.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.nodeRewardInterval !== 'number') {
+      response.success = false
+      response.reason = 'tx "next parameter nodeRewardInterval" field must be a number.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.nodeRewardAmountUsd !== 'bigint') {
+      response.success = false
+      response.reason = 'tx "next parameter nodeRewardAmountUsd" field must be a bigint.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.nodePenaltyUsd !== 'bigint') {
+      response.success = false
+      response.reason = 'tx "next parameter nodePenaltyUsd" field must be a bigint.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.stakeRequiredUsd !== 'bigint') {
+      response.success = false
+      response.reason = 'tx "next parameter stakeRequiredUsd" field must be a bigint.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.maintenanceInterval !== 'number') {
+      response.success = false
+      response.reason = 'tx "next parameter maintenanceInterval" field must be a number.'
+      throw new Error(response.reason)
+    }
+    if (typeof this.next.maintenanceFee !== 'number') {
+      response.success = false
+      response.reason = 'tx "next parameter maintenanceFee" field must be a number.'
+      throw new Error(response.reason)
+    }
+    return response
   }
-  return dapp.createWrappedResponse(accountId, accountCreated, account.hash, account.timestamp, account)
+
+  validate(_wrappedStates: WrappedStates, response: IncomingTransactionResult): IncomingTransactionResult {
+    response.success = true
+    response.reason = 'This transaction is valid!'
+    return response
+  }
+
+  apply(this: ApplyTally, txTimestamp: number, wrappedStates: WrappedStates, dapp: Shardus): void {
+    const network: DaoGlobalAccount = wrappedStates[daoConfig.daoAccount].data
+    network.next = this.next
+    network.nextWindows = this.nextWindows
+    network.timestamp = txTimestamp
+    dapp.log(`APPLIED TALLY GLOBAL ${stringify(network)} ===`)
+  }
+
+  keys(result: TransactionKeys): TransactionKeys {
+    result.targetKeys = [daoConfig.daoAccount]
+    result.allKeys = [...result.sourceKeys, ...result.targetKeys]
+    return result
+  }
+
+  createRelevantAccount(dapp: Shardus, account: DaoGlobalAccount, accountId: string, accountCreated = false): WrappedResponse {
+    if (!account) {
+      throw new Error('Network Account must already exist for the apply_tally transaction')
+    }
+    return dapp.createWrappedResponse(accountId, accountCreated, account.hash, account.timestamp, account)
+  }
 }
