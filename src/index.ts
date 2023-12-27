6249bacf359a4423c939156eaa1244958298a837
Kaung Myattthu <2962000kaungmyatthu@gmail.com>
2023-12-27 15:43:54 +0700

    fix: make use of exposed validity bool
    fix: remove redundant expensive ecrecover call

--- src/index.ts
+++ src/index.ts
@@ -84,7 +84,7 @@ import {
   generateTxId,
   isWithinRange,
   isValidVersion,
-  getTxSenderAddress,
+  getTxValiditySenderAddress,
 } from './utils'
 import config, { Config } from './config'
 import { daoConfig } from './config/dao'
