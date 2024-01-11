39d7d03c8dc26fa0808b7ee89a81b4777c0772e0
Arham Jain <arham.nec@gmail.com>
2024-01-11 17:19:21 +0530

    feat(code): added check to avoid invalid errors

--- src/index.ts
+++ src/index.ts
@@ -1370,7 +1370,7 @@ const configShardusEndpoints = (): void => {
 
       const wrappedCodeAccount = codeAccount.data as WrappedEVMAccount
       fixDeserializedWrappedEVMAccount(wrappedCodeAccount)
-      const contractCode = wrappedCodeAccount ? bytesToHex(wrappedCodeAccount.codeByte) : '0x'
+      const contractCode = wrappedCodeAccount.codeByte ? bytesToHex(wrappedCodeAccount.codeByte) : '0x'
       return res.json({ contractCode })
     } catch (error) {
       /* prettier-ignore */ if (logFlags.dapp_verbose) console.log('eth_getCode: ' + formatErrorMessage(error))
