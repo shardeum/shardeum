9af361c22ded5660986f867e56b82a37bbdaa7c4
andrew <3023784-afost@users.noreply.gitlab.com>
2024-01-21 21:57:18 -0600

    use network 1.10.0 settings

--- src/shardeum/initialNetworkParameters.ts
+++ src/shardeum/initialNetworkParameters.ts
@@ -15,9 +15,9 @@ export const initialNetworkParamters: NetworkParameters = {
   stakeRequiredUsd: oneSHM * BigInt(10),
   maintenanceInterval: ONE_DAY,
   maintenanceFee: 0,
-  minVersion: '1.9.0',
-  activeVersion: '1.9.0',
-  latestVersion: '1.9.1',
+  minVersion: '1.10.0',
+  activeVersion: '1.10.0',
+  latestVersion: '1.10.0',
   archiver: {
     minVersion: '3.4.2',
     activeVersion: '3.4.2',
