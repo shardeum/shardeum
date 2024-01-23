f87efbc62f9817787b58168f2409be0833908664
municorn <municorn@musicaloft.com>
2024-01-23 11:29:06 -0600

    feat(dao): use `isDaoTx` in `isInternalTx`

--- src/setup/helpers.ts
+++ src/setup/helpers.ts
@@ -36,7 +36,7 @@ export function isInternalTXGlobal(internalTx: InternalTx): boolean {
 
 export function isInternalTx<T extends { isInternalTx?: unknown }>(tx: T): boolean {
   console.log("daoLogging: isInternalTx", !!tx.isInternalTx || tx instanceof DaoTx)
-  return !!tx.isInternalTx || tx instanceof DaoTx
+  return !!tx.isInternalTx || isDaoTx(tx)
 }
 
 export function isDebugTx<T extends { isDebugTx?: unknown }>(tx: T): boolean {
