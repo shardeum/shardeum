fb526ad381adb1ce7660b18a0b350d71c09c60b4
municorn <municorn@musicaloft.com>
2024-01-23 11:27:54 -0600

    feat(dao): move and improve `isDaoTx`

--- src/setup/helpers.ts
+++ src/setup/helpers.ts
@@ -6,7 +6,7 @@ import {
 } from '@ethereumjs/tx'
 import * as crypto from '@shardus/crypto-utils'
 import { toBuffer, ToBufferInputTypes } from 'ethereumjs-util'
-import { DaoTx } from '../dao/tx'
+import { DaoTx, isDaoTx } from '../dao/tx'
 import { ShardeumFlags } from '../shardeum/shardeumFlags'
 import { InternalTx, InternalTXType } from '../shardeum/shardeumTypes'
 import { cryptoStringify } from '../utils/stringify'
