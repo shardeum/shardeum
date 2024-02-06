cd11440f2e0d63bae73db5d69ee05a3c0c1befaa
andrew <3023784-afost@users.noreply.gitlab.com>
2024-02-06 01:38:57 -0600

    use latest shardus core and archive server for prerc1

--- src/shardeum/initialNetworkParameters.ts
+++ src/shardeum/initialNetworkParameters.ts
@@ -19,9 +19,9 @@ export const initialNetworkParamters: NetworkParameters = {
   activeVersion: '1.10.0',
   latestVersion: '1.10.0',
   archiver: {
-    minVersion: '3.4.5',
-    activeVersion: '3.4.5',
-    latestVersion: '3.4.5',
+    minVersion: '3.4.6',
+    activeVersion: '3.4.6',
+    latestVersion: '3.4.6',
   },
   stabilityScaleMul: 1000,
   stabilityScaleDiv: 1000,
