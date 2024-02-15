847f0bcdf868e9d751d288ed9efa31a1ba79c457
andrew <3023784-afost@users.noreply.gitlab.com>
2024-02-14 19:58:58 -0600

    use latest shardus core and archiver

--- src/shardeum/initialNetworkParameters.ts
+++ src/shardeum/initialNetworkParameters.ts
@@ -19,9 +19,9 @@ export const initialNetworkParamters: NetworkParameters = {
   activeVersion: '1.10.0',
   latestVersion: '1.10.0',
   archiver: {
-    minVersion: '3.4.7',
-    activeVersion: '3.4.7',
-    latestVersion: '3.4.7',
+    minVersion: '3.4.8',
+    activeVersion: '3.4.8',
+    latestVersion: '3.4.8',
   },
   stabilityScaleMul: 1000,
   stabilityScaleDiv: 1000,
