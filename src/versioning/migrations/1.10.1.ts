0a8840fbc009814289e4c2755dd5e6db30e6b25a
Ahmed <iaahmed40@gmail.com>
2024-02-21 18:17:00 -0600

    added versioning for 1.10.1

---
+++ src/versioning/migrations/1.10.1.ts
@@ -0,0 +1,19 @@
+import { nestedCountersInstance } from '@shardus/core'
+//import { ShardeumFlags } from '../../shardeum/shardeumFlags'
+import { Migration } from '../types'
+import { shardusConfig } from '../..'
+
+// This has been baked into settings and is not needed, but the goal is to keep one migration as
+// an example for when we need to migrate again.
+
+export const migrate: Migration = async () => {
+  console.log('migrate 1.10.1')
+  nestedCountersInstance.countEvent('migrate', 'calling migrate 1.10.1')
+
+  //this enables the standby list to use a new form of calculation
+  shardusConfig.p2p.sortSyncFinished = true
+
+}
+
+//WARNING if you add a new one of these migration files you must add it to the migrations list in
+// src/versioning/index.ts
