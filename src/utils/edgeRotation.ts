d82d378e247ab8aa25786228703c0b054a994497
CombatPug <pe.ladar@icloud.com>
2024-04-11 13:44:02 -0300

    refactor out of bounds code

--- src/utils/edgeRotation.ts
+++ src/utils/edgeRotation.ts
@@ -1,4 +1,5 @@
 import config from '../config'
+import {nestedCountersInstance, Shardus} from '@shardus/core'
 
 export function isNodeRecentlyRotatedIn(idx: number, numActiveNodes: number): boolean {
   return (
@@ -14,4 +15,22 @@ export function isNodeNearRotatingOut(idx: number, numActiveNodes: number): bool
     config.server.p2p.rotationEdgeToAvoid &&
     idx >= numActiveNodes - config.server.p2p.rotationEdgeToAvoid
   )
+}
+export function isNodeOutOfRotationBounds(
+  shardus: Shardus,
+  nodeId: string
+): boolean {
+
+  const { idx, total } = shardus.getNodeRotationIndex(nodeId)
+  // skip freshly rotated in nodes
+  if (isNodeRecentlyRotatedIn(idx, total)) {
+    nestedCountersInstance.countEvent('skip-newly-rotated-node', nodeId)
+    return false
+  }
+
+  // skip about to be rotated out nodes
+  if (isNodeNearRotatingOut(idx, total)) {
+    nestedCountersInstance.countEvent('skip-about-to-rotate-out-node', nodeId)
+    return false
+  }
 }
