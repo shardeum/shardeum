677f562716e73424d6eb0e55c48bc9cdf379a65a
andrew <44451818+afostr@users.noreply.github.com>
2024-05-16 15:02:24 -0500

    adding configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -257,6 +257,9 @@ config = merge(config, {
       useNewPOQ: true, // 1.10.0 enabled required by archive server updates
 
       forwardToLuckyNodes: false, // 1.11.0 we seem to have more issues with this on.  can turn off for local testing
+
+      removeStuckTxsFromQueue: false,
+      removeStuckChallengedTXs: false,
     },
   },
 })
