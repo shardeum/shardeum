3b24d1117513be48e17245cc02f1ee1dbf497c49
Arham Jain <arham.nec@gmail.com>
2024-06-28 19:04:00 +0530

    feat(version): added 1.11.3 in versions/index

--- src/versioning/index.ts
+++ src/versioning/index.ts
@@ -17,7 +17,7 @@ const appliedMigrations = new Set<string>()
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const onActiveVersionChange = async (newActiveVersion: string) => {
   // For future migrations, add a file under ./migrations and add the version here
-  const migrations = ['1.9.1', '1.10.2', '1.11.2']
+  const migrations = ['1.9.1', '1.10.2', '1.11.2', '1.11.3']
 
   for (let index = 0; index < migrations.length; index++) {
     const migrationVersion = migrations[index] // eslint-disable-line security/detect-object-injection
