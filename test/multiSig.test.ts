47d97dd1179fd2001ea43e248caecfa55cb8de00
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2024-07-12 20:54:15 +0700

    fix: more test and backward compitable readable code

--- test/multiSig.test.ts
+++ test/multiSig.test.ts
@@ -1,11 +1,13 @@
-import { DevSecurityLevel, shardusFactory } from "@shardus/core"
+import { DevSecurityLevel } from "@shardus/core"
 import * as crypto from "@shardus/crypto-utils"
-import { verifyMultiSig } from "../src/setup/helpers"
+import { verifyMultiSigs } from "../src/setup/helpers"
+import { Utils } from "@shardus/types"
 
 crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc')
+
+crypto.setCustomStringifier(Utils.safeStringify, 'shardus_safeStringify')
 
 const KEYPAIRS: crypto.Keypair[]  = new Array(10)
-crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc')
 
 for(let i = 0; i < 10; i++) {
   KEYPAIRS[i] = crypto.generateKeypair()
@@ -37,7 +39,7 @@ test('should validate the internal tx with multi sig', () => {
     [KEYPAIRS[1].publicKey]: DevSecurityLevel.High,
     [KEYPAIRS[2].publicKey]: DevSecurityLevel.High
   }
-  const result = verifyMultiSig(
+  const result = verifyMultiSigs(
     changeConfigTx,
     sign,
     mockAllowedDevKeys,
@@ -63,7 +65,7 @@ test('should fail without min approval', ()=>{
     [KEYPAIRS[1].publicKey]: DevSecurityLevel.High,
     [KEYPAIRS[2].publicKey]: DevSecurityLevel.High
   }
-  const result = verifyMultiSig(
+  const result = verifyMultiSigs(
     changeConfigTx,
     sign,
     mockAllowedDevKeys,
@@ -89,7 +91,7 @@ test('should fail without min security level', ()=>{
     [KEYPAIRS[1].publicKey]: DevSecurityLevel.Medium,
     [KEYPAIRS[2].publicKey]: DevSecurityLevel.High
   }
-  const result = verifyMultiSig(
+  const result = verifyMultiSigs(
     changeConfigTx,
     sign,
     mockAllowedDevKeys,
@@ -114,7 +116,7 @@ test('should fail when pubkey is not recognized', ()=>{
     [KEYPAIRS[8].publicKey]: DevSecurityLevel.High,
     [KEYPAIRS[9].publicKey]: DevSecurityLevel.Medium,
   }
-  const result = verifyMultiSig(
+  const result = verifyMultiSigs(
     changeConfigTx,
     sign,
     mockAllowedDevKeys,
@@ -129,13 +131,13 @@ test('should fail when signature is invalid', ()=>{
   const sign: any = []
   const signature = crypto.sign(tx_hash, KEYPAIRS[0].secretKey)
   sign.push({
-    owner: KEYPAIRS[0].publicKey,
-    sig: signature.replace('a', 'b').replace('1','3') // temper with the signature signature
+    owner: KEYPAIRS[2].publicKey,
+    sig: signature // temper with the signature signature
   })
   const mockAllowedDevKeys = {
     [KEYPAIRS[0].publicKey]: DevSecurityLevel.High,
   }
-  const result = verifyMultiSig(
+  const result = verifyMultiSigs(
     changeConfigTx,
     sign,
     mockAllowedDevKeys,
@@ -145,3 +147,29 @@ test('should fail when signature is invalid', ()=>{
 
   expect(result).toBe(false)
 })
+
+test('backward compitible with single sig payloads', ()=>{
+  const tx_copy = changeConfigTx
+  const tx_hash = crypto.hashObj(changeConfigTx)
+  const hashed_sig = crypto.sign(tx_hash, KEYPAIRS[0].secretKey)
+
+  crypto.signObj(tx_copy, KEYPAIRS[0].secretKey, KEYPAIRS[0].publicKey)
+
+  const mockAllowedDevKeys = {
+    [KEYPAIRS[0].publicKey]: DevSecurityLevel.High,
+  }
+  const {sign, ...txWithoutSign} = tx_copy
+  const result = verifyMultiSigs(
+    txWithoutSign,
+    [tx_copy.sign],
+    mockAllowedDevKeys,
+    1,
+    DevSecurityLevel.High
+  )
+  
+  expect(result).toBe(true)
+  
+  expect(tx_copy.sign.sig).toBe(hashed_sig)
+
+})
+
