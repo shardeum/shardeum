b6a58157efb236161f02d69dba97398ceac84a0e
urnotsam <samwsweet@gmail.com>
2024-08-01 09:46:57 -0400

    updates to use new event data structure

--- src/index.ts
+++ src/index.ts
@@ -7142,7 +7142,7 @@ const shardusSetup = (): void => {
               publicKey: data.publicKey,
               nodeId: data.nodeId,
             }
-            shardus.addNetworkTx('rewardTx', txData)
+            shardus.addNetworkTx('nodeReward', txData)
           }
         }
       }
@@ -7229,13 +7229,12 @@ const shardusSetup = (): void => {
         console.log('shardeum-event', `try-network-transaction`, safeStringify(data))
         nestedCountersInstance.countEvent('shardeum-event', `try-network-transaction`)
         if (data?.additionalData.type === 'nodeReward') {
-          data = data.additionalData
           console.log(
             'shardeum-event',
             `running injectClaimrewardTxWithRetry nodeReward`,
             safeStringify(data)
           )
-          const closestNodes = shardus.getClosestNodes(data.txData.publicKey, 5)
+          const closestNodes = shardus.getClosestNodes(data.publicKey, 5)
           const ourId = shardus.getNodeId()
           for (const id of closestNodes) {
             if (id === ourId) {
