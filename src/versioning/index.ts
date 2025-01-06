735d2a45f4fd82668431c11a3db5ec29984a4710
Ahmed <iaahmed40@gmail.com>
2025-01-06 09:34:28 -0600

    update migration to 1.16.3

--- src/versioning/index.ts
+++ src/versioning/index.ts
@@ -17,7 +17,7 @@ const appliedMigrations = new Set<string>()
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const onActiveVersionChange = async (newActiveVersion: string) => {
   // For future migrations, add a file under ./migrations and add the version here
-  const migrations = ['1.9.1', '1.10.2', '1.11.2', '1.11.3', '1.15.4', '1.15.5']
+  const migrations = ['1.9.1', '1.10.2', '1.11.2', '1.11.3', '1.15.4', '1.16.3']
 
   for (let index = 0; index < migrations.length; index++) {
     const migrationVersion = migrations[index] // eslint-disable-line security/detect-object-injection
