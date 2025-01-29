54ab78a0e6c77cc46f5599fab58f5bb38227d6ee
Bob <158583129+bouchmann@users.noreply.github.com>
2025-01-29 23:21:24 +0100

    Update shardeumTypes.ts

--- src/shardeum/shardeumTypes.ts
+++ src/shardeum/shardeumTypes.ts
@@ -235,7 +235,7 @@ export interface NodeRefutedViolationData {
 
 export interface PenaltyTX extends InternalTxBase {
   reportedNodeId: string
-  reportedNodePublickKey: string
+  reportedNodepublicKey: string
   operatorEVMAddress: string
   violationType: ViolationType
   violationData: LeftNetworkEarlyViolationData | SyncingTimeoutViolationData | NodeRefutedViolationData
