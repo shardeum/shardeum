69b30581a90773feae6c7994393cda689f21674b
urnotsam <samwsweet@gmail.com>
2025-02-14 17:23:32 -0500

    use type any instead of opaquetransaction

--- src/index.ts
+++ src/index.ts
@@ -7974,11 +7974,12 @@ const shardusSetup = (): void => {
     ): boolean => {
       return verifyMultiSigs(rawPayload, sigs, allowedPubkeys, minSigRequired, requiredSecurityLevel)
     },
-    isNGT: (acceptedTx: OpaqueTransaction): boolean => {
+    isNGT: (acceptedTx: any): boolean => {
       const INIT_REWARD_TX = 8
       const CLAIM_REWARD_TX = 9
       const NGT_TYPES = [INIT_REWARD_TX, CLAIM_REWARD_TX]
       return NGT_TYPES.includes(acceptedTx.data?.tx?.['internalTXType'])
+      return NGT_TYPES.includes(acceptedTx?.data?.tx?.['internalTXType'])
     },
   })
 
