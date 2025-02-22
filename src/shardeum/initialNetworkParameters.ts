53b1a7cb156a90801fc48585caa053e5390c49e6
andrew <44451818+afostr@users.noreply.github.com>
2025-02-21 20:41:44 -0600

    use latest core and types

--- src/shardeum/initialNetworkParameters.ts
+++ src/shardeum/initialNetworkParameters.ts
@@ -16,9 +16,9 @@ export const initialNetworkParamters: NetworkParameters = {
   restakeCooldown: THIRTY_MINUTES,
   maintenanceInterval: ONE_DAY,
   maintenanceFee: 0,
-  minVersion: '1.16.3',
+  minVersion: '1.16.4',
   activeVersion: '1.16.1',
-  latestVersion: '1.16.4',
+  latestVersion: '1.16.5',
   archiver: {
     minVersion: '3.5.6',
     activeVersion: '3.5.6',
