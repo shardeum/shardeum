109fb089a3383bbba5fb639b723c1cb1be0ead4f
Amitesh Thind <thindamitesh@gmail.com>
2025-05-02 00:35:09 -0400

    updated test config to show coverage result and easy reference to src directory

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 5000000, // the more node involve in testing, the higher the timeout requires
   verbose: true,
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1', // Map @src to the src folder
+  },
   roots: ['<rootDir>/test/unit'],
   setupFiles: ['<rootDir>/test/unit/setup.ts'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
@@ -11,4 +14,5 @@ module.exports = {
   },
   resetMocks: true,
   clearMocks: true,
+  collectCoverage: true,
 }
