153605bcb61404bf787971f5e690d09daf9f12af
Zachary Belford <belfordz66@gmail.com>
2025-05-20 16:16:12 -0600

    Replace shardeum state map with LRU cache

--- src/debug/evmSetup/index.ts
+++ src/debug/evmSetup/index.ts
@@ -3,6 +3,7 @@ import { ShardeumFlags } from '../../shardeum/shardeumFlags'
 import { ShardeumBlock } from '../block/blockchain'
 import { VM } from '../../vm_v7/vm'
 import { ShardeumState } from '../state'
+import { LRUCache } from 'lru-cache'
 import { EVMAccountInfo } from '../../shardeum/shardeumTypes'
 import { ShardusTypes } from '@shardeum-foundation/core'
 import { EVM as EthereumVirtualMachine } from '../../evm_v2'
@@ -10,7 +11,7 @@ import { EVM as EthereumVirtualMachine } from '../../evm_v2'
 let shardeumBlock: ShardeumBlock
 export let evmCommon: Common
 export let EVM: { -readonly [P in keyof VM] }
-export let shardeumStateTXMap: Map<string, ShardeumState>
+export let shardeumStateTXMap: LRUCache<string, ShardeumState>
 export let shardusAddressToEVMAccountInfo: Map<string, EVMAccountInfo>
 export let debugAppdata: Map<string, unknown>
 
@@ -84,8 +85,11 @@ export async function initEVMSingletons(): Promise<void> {
   //todo need to evict old data
   ////transactionStateMap = new Map<string, TransactionState>()
 
-  // a map of txID or ethcallID to shardeumState, todo need to evict old data
-  shardeumStateTXMap = new Map<string, ShardeumState>()
+  // a map of txID or ethcallID to shardeumState
+  shardeumStateTXMap = new LRUCache<string, ShardeumState>({
+    max: 1000,
+    ttl: 60_000,
+  })
   // a map of txID or ethcallID to shardeumState, todo need to evict old data
   //shardeumStateCallMap = new Map<string, ShardeumState>()
 
