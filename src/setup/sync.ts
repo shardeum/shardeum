442919bc234a252d32ad1414e84a17291027941e
Zachary Belford <belfordz66@gmail.com>
2025-05-20 17:12:14 -0600

    fix: make tryGetRemoteAccountCBNoOp async

--- src/setup/sync.ts
+++ src/setup/sync.ts
@@ -398,13 +398,13 @@ function contractStorageInvolvedNoOp(
   return true
 }
 
-function tryGetRemoteAccountCBNoOp(
+async function tryGetRemoteAccountCBNoOp(
   _transactionState: TransactionState,
   _type: AccountType,
   _address: string,
   _key: string
 ): Promise<WrappedEVMAccount> {
-  return undefined
+  return Promise.resolve(undefined)
 }
 /* eslint-enable @typescript-eslint/no-unused-vars */
 
