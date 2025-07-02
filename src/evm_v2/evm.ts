5e2eaa8ab7e90b7011343fcc8816c896bd9a1733
Sam Sweet <samwsweet@gmail.com>
2025-07-02 11:05:41 -0400

    add 7516 to supportedEIPs

--- src/evm_v2/evm.ts
+++ src/evm_v2/evm.ts
@@ -157,7 +157,7 @@ export class EVM implements EVMInterface {
     // Supported EIPs
     const supportedEIPs = [
       1153, 1559, 2315, 2565, 2718, 2929, 2930, 3074, 3198, 3529, 3540, 3541, 3607, 3651, 3670, 3855, 3860, 4399, 4895,
-      4788, 4844, 5133, 5656, 6780,
+      4788, 4844, 5133, 5656, 6780, 7516,
     ]
 
     for (const eip of this.common.eips()) {
