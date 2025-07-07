cf9e6fa8cf7ad1c5d7208d31b27af2b21179cc30
devendra-shardeum <devendra@shardeum.org>
2025-07-08 04:04:11 +0530

    fix issue in blake2f precompile

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,14 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 5000000, // the more node involve in testing, the higher the timeout requires
   verbose: true,
-  roots: ['<rootDir>/test/unit'],
+  roots: ['<rootDir>/test/unit','<rootDir>/test/testCases'],
   setupFiles: ['<rootDir>/test/unit/setup.ts'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   resetMocks: true,
   clearMocks: true,
