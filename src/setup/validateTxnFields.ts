121819f70db7b4ce7125d4d47e33b3cc6e95cfc3
Marc Hanson <marc.hanson.github@gmail.com>
2025-07-25 19:57:01 -0400

    Prettier'd

--- src/setup/validateTxnFields.ts
+++ src/setup/validateTxnFields.ts
@@ -459,7 +459,12 @@ export const validateTxnFields =
         }
 
         const isStakeRelatedTx: boolean = isStakingEVMTx(transaction)
-        if (!AccountsStorage.cachedNetworkAccount.current.smartContractSupport && appData && !appData.internalTx && !isStakeRelatedTx) {
+        if (
+          !AccountsStorage.cachedNetworkAccount.current.smartContractSupport &&
+          appData &&
+          !appData.internalTx &&
+          !isStakeRelatedTx
+        ) {
           const isCoinTransfer =
             transaction.value != null &&
             transaction.to != null &&
