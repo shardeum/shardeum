d95de21bfcba60584c7c639411361bb79c0a4395
Marc Hanson <marc.hanson.github@gmail.com>
2025-09-03 16:30:02 -0400

    Bump initial network version

--- src/shardeum/initialNetworkParameters.ts
+++ src/shardeum/initialNetworkParameters.ts
@@ -19,7 +19,7 @@ export const initialNetworkParamters: NetworkParameters = {
   maintenanceFee: 0,
   minVersion: '1.19.0',
   activeVersion: '1.19.0',
-  latestVersion: '1.19.4',
+  latestVersion: '1.19.5',
   archiver: {
     minVersion: '3.6.0-prerelease.0',
     activeVersion: '3.6.0-prerelease.0',
