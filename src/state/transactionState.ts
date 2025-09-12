3646a2ec4d998609987d946b098914bbd937a3c4
Jintu Das <jintuisbusy@gmail.com>
2025-09-12 19:20:32 +0530

    Fix account stack pop bug

--- src/state/transactionState.ts
+++ src/state/transactionState.ts
@@ -554,10 +554,11 @@ export default class TransactionState {
 
     if (this.debugTrace) this.debugTraceLog(`putAccount: addr:${addressString} v:${Utils.safeStringify(accountObj)}`)
 
-    //this.allAccountWrites.set(addressString, storedRlp)
-
-    //this.checkpoints[this.checkpoints.length - 1]
-    if (this.allAccountWritesStack.length > 0) {
+    // write to allAccountWrites when checkpointing is enabled
+    // The checkpoint system preserves allAccountWrites in the stack for revert
+    if (ShardeumFlags.CheckpointRevertSupport && this.checkpointCount > 0) {
+      this.allAccountWrites.set(addressString, storedRlp)
+    } else if (this.allAccountWritesStack.length > 0) {
       const accountWrites = this.allAccountWritesStack[this.allAccountWritesStack.length - 1]
       accountWrites.set(addressString, storedRlp)
     } else {
@@ -1011,8 +1012,8 @@ export default class TransactionState {
     }
 
     //we need checkpoint / revert stack support for accounts so that gas is handled correctly
-    //this.allAccountWritesStack.push(this.allAccountWrites)
-    this.allAccountWritesStack.push(new Map<string, Uint8Array>())
+    // Save current state to stack before clearing for new changes
+    this.allAccountWritesStack.push(new Map(this.allAccountWrites))
 
     // Also checkpoint contract bytecode writes
     this.allContractBytesWritesStack.push(new Map(this.allContractBytesWrites))
@@ -1109,7 +1110,6 @@ export default class TransactionState {
 
       if (this.allAccountWritesStack.length > 0) {
         this.allAccountWrites = this.allAccountWritesStack.pop()
-        this.allAccountWrites.clear()
       } else {
         this.allAccountWrites.clear()
       }
